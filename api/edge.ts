c1ef8998c9650cbbf747c268ac021d8c760e3c47
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-05-04 14:55:53 +0900

    rm unused console.log

--- api/edge.ts
+++ api/edge.ts
@@ -6,7 +6,6 @@ export const config = {
 
 export default function handler(req: Request) {
   const url = new URL(req.url);
-  console.log({ url });
   const { city } = geolocation(req);
 
   return new Response(`Hello, from ${city} I'm now an Edge Function!`);
